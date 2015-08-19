b6e7a4419db53bb18d7adc528532a737ecf4e4e8
Rajasekhara <sekharreddy.arimadna@gmail.com>
2015-08-19 16:24:59 +0530

    asdasd

--- JesryRestTest/src/com/tag/rest/Test.java
+++ JesryRestTest/src/com/tag/rest/Test.java
@@ -6,19 +6,12 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/test/{param:[A-Z]*}")
+@Path("/test/{param}")
 public class Test {
 
-	/*@GET
-	@Produces(MediaType.TEXT_HTML)
-	public String getMessage(@PathParam("param") String message) {  
-		  String output = "Jersey say Hello World!!! : " + message;  
-		 // return Response.status(200).entity(output).build();
-		  return output;
-		 } */ 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public String getMessage1(@PathParam("param") String message) {  
+	public String getJson(@PathParam("param") String message) { 
 		  String output = "Jersey say Hello World!!! : " + message;  
 		 // return Response.status(200).entity(output).build();
 		  return output;
